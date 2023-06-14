7f9dafe18765a9cd8848c0c393c18f31aa7317b9
scott.hsieh[謝書正] <scott.hsieh@scotthsieh-mac13.local>
2023-06-14 16:54:54 +0800

    Add DynamoDB construct

--- lib/dynamodb-streaming-datalake-stack.ts
+++ lib/dynamodb-streaming-datalake-stack.ts
@@ -3,7 +3,7 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as kinesis from 'aws-cdk-lib/aws-kinesis';
 import * as kms from 'aws-cdk-lib/aws-kms';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 interface DynamodbStreamingDatalakeStackProps extends cdk.StackProps {
@@ -32,6 +32,23 @@ export class DynamodbStreamingDatalakeStack extends cdk.Stack {
             encryptionKey: datalakeBucketKey
         });
 
+        const exampleDdbTable = new dynamodb.Table(this, 'ExampleDdbTable', {
+            tableName: 'example-ddb-table',
+            kinesisStream: ddbStream,
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+            pointInTimeRecovery: true,
+            partitionKey: {
+                name: 'id',
+                type: dynamodb.AttributeType.STRING
+            },
+            sortKey: {
+                name: 'name',
+                type: dynamodb.AttributeType.STRING
+            },
+            encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
+            encryptionKey: datalakeBucketKey
+        });
+
         // The code that defines your stack goes here
 
         // example resource
@@ -40,6 +57,7 @@ export class DynamodbStreamingDatalakeStack extends cdk.Stack {
         // });
         new cdk.CfnOutput(this, 'DatalakeBucketArn', { value: datalakeBucket.bucketArn, description: 'Datalake Bucket ARN' });
         new cdk.CfnOutput(this, 'DdbStreamArn', { value: ddbStream.streamArn, description: 'The ARN of the Kinesis Stream for DynamoDB' });
-        new cdk.CfnOutput(this, 'DataLakeBucket', { value: datalakeBucketKey.keyArn })
+        new cdk.CfnOutput(this, 'DataLakeBucketKeyArn', { value: datalakeBucketKey.keyArn });
+        new cdk.CfnOutput(this, 'DdbTableArn', { value: exampleDdbTable.tableArn, description: 'The ARN of the DynamoDB table' })
     }
 }
