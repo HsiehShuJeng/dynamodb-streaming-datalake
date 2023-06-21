7fce80ae149bd12fd60ea1c3026aa08304df3a21
scott.hsieh[謝書正] <scott.hsieh@scotthsieh-mac13.local>
2023-06-21 10:49:55 +0800

    Add kinese firehose

--- lib/dynamodb-streaming-datalake-stack.ts
+++ lib/dynamodb-streaming-datalake-stack.ts
@@ -4,6 +4,9 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as kinesis from 'aws-cdk-lib/aws-kinesis';
 import * as kms from 'aws-cdk-lib/aws-kms';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as kinesisfirehouse from 'aws-cdk-lib/aws-kinesisfirehose';
+import * as logs from 'aws-cdk-lib/aws-logs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 interface DynamodbStreamingDatalakeStackProps extends cdk.StackProps {
@@ -29,7 +32,7 @@ export class DynamodbStreamingDatalakeStack extends cdk.Stack {
             streamName: 'ddb-exclusive-stream',
             shardCount: 10,
             encryption: kinesis.StreamEncryption.KMS,
-            encryptionKey: datalakeBucketKey
+            encryptionKey: datalakeBucketKey,
         });
 
         const exampleDdbTable = new dynamodb.Table(this, 'ExampleDdbTable', {
@@ -48,6 +51,124 @@ export class DynamodbStreamingDatalakeStack extends cdk.Stack {
             encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
             encryptionKey: datalakeBucketKey
         });
+
+        const firehoseDeliveryRole = new iam.Role(this, 'FirehoseDeliveryRole', {
+            roleName: 'firehose-dbstream-role',
+            assumedBy: new iam.ServicePrincipal('firehose.amazonaws.com'),
+            description: 'Role for Firehose to deliver data to S3',
+            inlinePolicies: {
+                ['AllowLogging']: new iam.PolicyDocument({
+                    assignSids: true,
+                    statements: [
+                        new iam.PolicyStatement({
+                            effect: iam.Effect.ALLOW,
+                            actions: [
+                                'logs:CreateLogGroup',
+                                'logs:CreateLogStream',
+                                'logs:PutLogEvents'
+                            ],
+                            resources: [
+                                `arn:${cdk.Aws.PARTITION}:logs:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:log-group:/aws/kinesisfirehose/*:*`
+                            ]
+                        })
+                    ]
+                }),
+                ['KmsPermissions']: new iam.PolicyDocument({
+                    assignSids: true,
+                    statements: [
+                        new iam.PolicyStatement({
+                            effect: iam.Effect.ALLOW,
+                            actions: [
+                                'kms:Encrypt',
+                                'kms:Decrypt',
+                                'kms:ReEncrypt',
+                                'kms:GenerateDataKey*',
+                                'kms:DescribeKey'
+                            ],
+                            resources: [
+                                datalakeBucketKey.keyArn
+                            ]
+                        })
+                    ]
+                }),
+                ['S3BucketPermissions']: new iam.PolicyDocument({
+                    assignSids: true,
+                    statements: [
+                        new iam.PolicyStatement({
+                            effect: iam.Effect.ALLOW,
+                            actions: [
+                                's3:ListBucket',
+                                's3:ListBucketByTags',
+                                's3:GetBucketLocation'
+                            ],
+                            resources: [
+                                datalakeBucket.bucketArn
+                            ]
+                        })
+                    ]
+                }),
+                ['S3ObjectPermissions']: new iam.PolicyDocument({
+                    assignSids: true,
+                    statements: [
+                        new iam.PolicyStatement({
+                            effect: iam.Effect.ALLOW,
+                            actions: [
+                                's3:PutObject',
+                                's3:ListObjects',
+                                's3:PutObjectAcl'
+                            ]
+                        })
+                    ]
+                }),
+                ['KinesisPermissions']: new iam.PolicyDocument({
+                    assignSids: true,
+                    statements: [
+                        new iam.PolicyStatement({
+                            effect: iam.Effect.ALLOW,
+                            actions: [
+                                'kinesis:DescribeStream',
+                                'kinesis:GetShardIterator',
+                                'kinesis:GetRecords',
+                                'kinesis:ListShards'
+                            ],
+                            resources: [
+                                ddbStream.streamArn
+                            ]
+                        })
+
+                    ]
+                })
+            }
+        });
+        const firehouseStreamName = 'ddb-table-firehose-delivery-stream';
+        const firehouseLogGroup = new logs.LogGroup(this, 'FirehouseLogGroup', {
+            logGroupName: `/aws/kinesisfirehose/${firehouseStreamName}`,
+            retention: logs.RetentionDays.THREE_MONTHS
+        })
+        new kinesisfirehouse.CfnDeliveryStream(this, 'DynamoDBFirehose', {
+            deliveryStreamName: firehouseStreamName,
+            deliveryStreamType: 'KinesisStreamAsSource',
+            kinesisStreamSourceConfiguration: {
+                kinesisStreamArn: ddbStream.streamArn,
+                roleArn: firehoseDeliveryRole.roleArn
+            },
+            extendedS3DestinationConfiguration: {
+                bucketArn: datalakeBucket.bucketArn,
+                bufferingHints: {
+                    intervalInSeconds: 60,
+                    sizeInMBs: 64
+                },
+                cloudWatchLoggingOptions: {
+                    enabled: true,
+                    logGroupName: firehouseLogGroup.logGroupName,
+                    logStreamName: 'S3Delivery'
+                },
+                compressionFormat: 'Snappy',
+                errorOutputPrefix: `error/${exampleDdbTable.tableName}/`,
+                prefix: `dynamodb/aws21/${exampleDdbTable.tableName}/!{timestamp:yyyy/MM/dd/}`,
+                roleArn: firehoseDeliveryRole.roleArn
+            }
+        })
 
         // The code that defines your stack goes here
 
